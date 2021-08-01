9af4ce50e21c0415c4e14952de4cffa895ccbb1b
Jake Witcher <jjwitcher09@gmail.com>
2021-07-31 20:21:04 -0400

    add benchmarks for day-1

--- day-1/test/elevator_test.go
+++ day-1/test/elevator_test.go
@@ -2,6 +2,7 @@ package elevator
 
 import (
 	"day-1/internal/elevator"
+	"day-1/internal/input"
 	"testing"
 )
 
@@ -18,5 +19,29 @@ func TestElevatorFindPosition(t *testing.T) {
 		if position, _ := elevator.FindPosition(test.route, test.floor); position != test.position {
 			t.Fatalf("floor position: %d, position position: %d", test.position, position)
 		}
+	}
+}
+
+func BenchmarkElevatorRun(b *testing.B) {
+	route, err := input.Extract("../internal/input/input.txt")
+	if err != nil {
+		b.Fatalf("unexpected error: %v", err)
+	}
+
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		_ = elevator.Run(route)
+	}
+}
+
+func BenchmarkElevatorFindPosition(b *testing.B) {
+	route, err := input.Extract("../internal/input/input.txt")
+	if err != nil {
+		b.Fatalf("unexpected error: %v", err)
+	}
+
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		_, _ = elevator.FindPosition(route, -1)
 	}
 }
